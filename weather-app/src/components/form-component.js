33488a4236ae5c85acc715f170e0ff5aac0104f5
jordan <jordan@jordans-MacBook-Air.local>
2020-02-18 21:50:57 +0700

    got error message to go away once fields are filled

--- weather-app/src/components/form-component.js
+++ weather-app/src/components/form-component.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./form-style.css";
+import "./form.style.css";
 
 const Form = props => {
   return (
@@ -25,7 +25,7 @@ const Form = props => {
               placeholder="Country"
             />
           </div>
-          <div className="col-md-3 mt-md-0 text-md-left">
+          <div className="col-md-3 mt-md-0 py-2 text-md-left">
             <button className="btn btn-warning">Get Weather</button>
           </div>
         </div>
